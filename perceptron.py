9044bbde91c6e8a3574e91580349f48d49c3d04f
Ton <ton.paes.bccs@gmail.com>
2019-04-04 23:59:35 -0300

    Fixing bug on errors_ array

--- perceptron.py
+++ perceptron.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 class Perceptron(object):
-    """ Perceptron classifier.
+    """
+    Perceptron classifier.
 
     Parameters
     -----------
@@ -40,20 +41,20 @@ class Perceptron(object):
         self.w_ = np.zeros(1 + X.shape[1])
         self.errors_ = []
 
-        for i in range(self.n_iter):
+        for xi in range(self.n_iter):
             errors = 0
             # passo de correção do erro
             for xi, target in zip(X, y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += (update * xi)
+                update = self.eta * target - self.predict(xi)
+                self.w_[1:] += update * xi
                 self.w_[0] += update
                 errors += int(update != 0.0)
-            self.erros_.append[errors]
+            self.errors_.append(errors)
         return self
 
     def net_input(self, X):
         """Calcule net input"""
-        return np.dot(X, self.w_[1:]) + self.w_[0]
+        return np.dot(X, self.w_[1:]) + self.w_[0:]
 
     def predict(self, X):
         """Return class label after unit step"""
